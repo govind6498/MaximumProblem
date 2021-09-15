2bca696df165630eb9337dc2d9eaf3bcc14dab7f
govind6498 <govindsingh6498@gmail.com>
2021-09-16 00:00:10 +0530

    Added:Find maximum number from three Integer

--- src/co/bridgelabz/maximumproblem/Maximum.java
+++ src/co/bridgelabz/maximumproblem/Maximum.java
@@ -1,7 +1,40 @@
 package co.bridgelabz.maximumproblem;
-
+import java.util.*;
 public class Maximum {
+	public static <E extends Comparable<E>> void findMaximum(E first, E second, E third) {
+
+		E maximum = first;
+
+		if(first.compareTo(second) == 1 ) {
+			if(first.compareTo(third) == 1) {
+				maximum = first;
+			}
+			else {
+				maximum = third;
+			}
+		}
+		else if(second.compareTo(first) == 1 ) {
+			if(second.compareTo(third) == 1) {
+				maximum = second;
+			}
+			else {
+				maximum = third;
+			}
+		}
+		System.out.println("Maximum: "+maximum);
+	}
+
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter First Integer: ");
+		Integer firstIntegerNumber = Integer.valueOf(sc.nextInt());
+		System.out.print("Enter Second Integer: ");
+		Integer secondIntegerNumber = Integer.valueOf(sc.nextInt());
+		System.out.print("Enter Third Integer: ");
+		Integer thirdIntegerNumber = Integer.valueOf(sc.nextInt());
+		findMaximum(firstIntegerNumber, secondIntegerNumber, thirdIntegerNumber);
+		sc.close();
 	}
 }
+
